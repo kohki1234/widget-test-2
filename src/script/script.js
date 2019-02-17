ac2cd7983e45e2b368e88e10338669c51468b80b
Kohki Sato <you@example.com>
2019-02-17 14:54:35 +0900

    first commit

---
+++ src/script/script.js
@@ -0,0 +1,26 @@
+
+
+function clickEvent(){
+    console.log("TEST");
+
+    var notificationHandler = function(data) {
+        // Do something with the notifications
+    };
+
+
+    var notifyWhenDone = function(err) {
+        if (err) {
+            // Do something with the error
+        }
+        // called when the command is completed successfully,
+        // or when the action terminated with an error.
+    };
+
+    var cmdName = lpTag.agentSDK.cmdNames.write; // = "Write ChatLine"
+    var data = {text: "Some text"};
+
+
+    lpTag.agentSDK.init({notificationCallback: notificationHandler});
+
+    lpTag.agentSDK.command(cmdName, data, notifyWhenDone);
+};
